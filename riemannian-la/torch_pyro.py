0f7b1323ba30e8a029cb7b8c512bfa3d13c6f847
simoneiriksson <simoneiriksson@gmail.com>
2024-10-29 09:08:21 +0100

    Building manifold class with riemannian LA

--- riemannian-la/torch_pyro.py
+++ riemannian-la/torch_pyro.py
@@ -13,6 +13,11 @@ from models import MyModel, mini, PyroModel
 from pandas.plotting import scatter_matrix
 import pandas as pd
 from pyro.infer import Predictive
+from scipy.integrate import solve_ivp
+
+from utils import set_weights_old, extract_parameters, set_weights
+from laplace import Laplace
+from torch.utils.data import DataLoader, TensorDataset
 
 def fn(xs):
     return torch.column_stack([xs[:,0], xs[:,1], xs[:,0]**2 + xs[:,1]**2])
@@ -21,14 +26,18 @@ def fn(xs):
     return xs
 
 def generate_data(N, M, fn, weights, log_scale=0.1):
-    xs = torch.rand((N, M))*10
+    #xs = torch.rand((N, M))*10
+    xs = torch.ones(N, M)
     #xs, _ = xs.sort(dim=0)
-    ys = fn(xs) @ weights + torch.normal(torch.tensor(0.0), torch.tensor(1.)*torch.tensor(log_scale).exp(), size=(xs.shape[0],1))
+    ys = fn(xs) @ weights + torch.normal(torch.tensor(0.0), torch.tensor(1.)*torch.tensor(log_scale).exp
+    (), size=(xs.shape[0],1))
+    ys = torch.normal(torch.tensor(0.0), torch.tensor(1.)*torch.tensor(log_scale).exp
+    (), size=(xs.shape[0],1))
     return xs, ys
 
 M=2
 N=10
-log_scale = torch.tensor(1.).log()
+log_scale = torch.tensor(.1).log()
 weights = torch.tensor([[1.0], [2.0]])   
 xs, ys = generate_data(N, M, fn, weights, log_scale=log_scale)
 perm = torch.randperm(N)
@@ -51,7 +60,7 @@ class MyModel(torch.nn.Module):
         self.paws = torch.arange(1, num_params+1)
         self.fn = fn
     def forward(self, xs):
-        return  (fn(xs) * self.weights**self.paws).sum().unsqueeze(-1)
+        return  ((fn(xs) * self.weights**self.paws).sum(dim=1) - 1).unsqueeze(-1)
 
 
 ################################################
@@ -68,7 +77,7 @@ pyromodel = PyroModel(model, prior_log_sigma=prior_log_sigma,
                       batch_size = None)
 
 nuts_kernel_a = NUTS(pyromodel.model, step_size=1.)
-mcmc_auto = MCMC(nuts_kernel_a, num_samples=100, warmup_steps=100)
+mcmc_auto = MCMC(nuts_kernel_a, num_samples=300, warmup_steps=100)
 mcmc_auto.run(x_train, y_train)
 mcmc_auto.summary()
 
@@ -108,44 +117,179 @@ posterior_samples = mcmc_auto.get_samples()['parameters_samples']
 ###################
 # Laplace approximation using laplace-torch
 model = MyModel(num_params=M, fn=fn, prior_log_sigma=prior_log_sigma)
-optimizer = torch.optim.Adam(model.parameters(), lr=.1)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
 criterion = torch.nn.MSELoss()
+epochs=50000
+train_loader = DataLoader(TensorDataset(x_train, y_train), batch_size=16)
+for epoch in range(epochs):
+    accum_loss = 0            
+    for batch_no, (x, y) in enumerate(train_loader):
+        optimizer.zero_grad()
+        pred = model(x)
+        loss = criterion(pred, y)
+        loss.backward()
+        optimizer.step()
+        accum_loss += loss.item()
+    if epoch % 100 == 0:
+        print(f"{epoch = }, {accum_loss = }\t\t ", end="\r")
 
-for i in range(50000):
-    accum_loss = 0
-    
-    optimizer.zero_grad()
-    x = x_train
-    y = y_train
-    optimizer.zero_grad()
-    loss = criterion(model(x), y)
-    accum_loss += loss.item()
-    loss.backward()
-    optimizer.step()
-    if i % 100 == 0:
-        print(f"{i = }, {accum_loss = }\t\t ", end="\r")
 
+MAP = torch.nn.utils.parameters_to_vector(model.parameters())
 
 print(f"{model.weights = }")
 
-from laplace import Laplace
 
 la = Laplace(model, likelihood="regression", hessian_structure="full", subset_of_weights='all')
-from torch.utils.data import DataLoader, TensorDataset
 train_loader = DataLoader(TensorDataset(x_train, y_train), batch_size=16)
 la.fit(train_loader)
-print(f"{la.posterior_scale/x_train.shape[0] = }")
-print(f"{la.posterior_covariance/x_train.shape[0] = }")
-#print(f"{la.H.inverse() = }")
+print(f"{la.posterior_covariance = }")
+print(f"{la.posterior_precision = }")
+
 print(f"{la.mean = }")
-
-
+print(f"{la.H = }")
+dir(la)
 fig, axs = distributions_plot(posterior_samples, la.mean, la.posterior_covariance)
 fig.show()
 
 
+class Manifold():
+    def __init__(self, model, likelihood, device="cpu"):
+        super(Manifold, self).__init__()
+        self.model = model.to(device)
+        self.likelihood = likelihood
+        self.device = device
+
+
+        if self.likelihood == "regression":
+            self.criterion = torch.nn.MSELoss()
+        elif self.likelihood == "classification":
+            self.criterion = torch.nn.CrossEntropyLoss()
+        else:
+            raise ValueError("Likelihood not recognized")
+
+        self.MAP = None
+        self.MAP_covariance = None
+        self.LA = Laplace(model, likelihood="regression", hessian_structure="full", subset_of_weights='all')
+        
+    def set_train_data(self, x_train, y_train):
+        self.x_train = x_train
+        self.y_train = y_train
+        self.train_loader = DataLoader(TensorDataset(self.x_train, self.y_train), batch_size=16)
+
+    def fit(self, epochs=1, lr=0.1, optimizer=None, verbose=False, print_every_epoch=100):
+        if optimizer == None:
+            optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
+
+        for epoch in range(epochs):
+            accum_loss = 0            
+            for batch_no, (x, y) in enumerate(self.train_loader):
+                optimizer.zero_grad()
+                x, y = x.to(self.device), y.to(self.device)
+                pred = model(x)
+                loss = self.criterion(pred, y)
+                loss.backward()
+                optimizer.step()
+                accum_loss += loss.item()
+            if verbose:
+                if epoch % print_every_epoch == 0:
+                    print(f"{epoch = }, {accum_loss = }\t\t ", end="\r")
+        self.MAP = torch.nn.utils.parameters_to_vector(model.parameters())
+        self.LA.fit(self.train_loader)
+        self.MAP_covariance = self.LA.posterior_covariance
+
+    def set_to_map(self):
+        if self.MAP is not None:
+            torch.nn.utils.vector_to_parameters(self.MAP, self.model.parameters())
+        else: raise ValueError("No MAP found")
+
+    def set_weights(self, theta):
+        torch.nn.utils.vector_to_parameters(theta, self.model.parameters())
+
+    def loss(self):
+        return criterion(self.model(self.x_train), self.y_train)
+
+    def gradient(self):
+        #params = torch.nn.utils.parameters_to_vector(self.model.parameters())
+        grads = torch.autograd.grad(self.loss(), self.model.parameters())
+        return torch.cat([parm.flatten() for parm in list(grads)])
+
+    def covariance(self):
+        self.LA.fit(self.train_loader)
+        return self.LA.posterior_covariance
+    
+    def hess(self):
+        self.LA.fit(self.train_loader)
+        return la.posterior_precision
+
+    def posterior_sample(self, n_samples=1):
+        return self.LA.sample(n_samples=n_samples)
+    
+    def predictive_samples(self, x, n_samples=1):
+        return self.LA.predictive_samples(x, n_samples=n_samples)
+
+    def ode_fun(self, t, state):
+        #print(f"{state = }")
+        state_tensor = torch.tensor(state, dtype=torch.float32).to(self.device)
+        theta = state_tensor[:self.MAP.shape[0]]
+        v = state_tensor[self.MAP.shape[0]:]
+        self.set_weights(theta)
+        grad_val = self.gradient()
+        hess_val = self.hess()
+        #print(f"{grad_val = }")
+        #print(f"{hess_val = }")
+        #print(f"{v = }")
+
+        acc = -(grad_val * (1 / (1 + grad_val.T @ grad_val)) * (v.T @ hess_val @ v)).flatten()
+        return torch.cat([v, acc]).to("cpu").detach().numpy()
+    
+    def expmap(self, theta, v):
+        init = torch.cat([theta, v]).to("cpu").detach().numpy()
+        solution = solve_ivp(self.ode_fun, [0, 1], init, dense_output=True, rtol=1e-3, atol=1e-3)
+        return solution
+
+    def metric(self):
+        D = self.MAP.shape[0]
+        Id = torch.eye(D)
+        Grad_val = self.gradient()
+        #Hess_val = self.hess()
+        M = Id + Grad_val * Grad_val.T
+        return M
 
 
 
+model = MyModel(num_params=M, fn=fn, prior_log_sigma=prior_log_sigma)
+mymanifold = Manifold(model, likelihood="regression")
+mymanifold.set_train_data(x_train, y_train)
 
+mymanifold.fit(epochs=5000, lr=0.1, verbose=True)
+mymanifold.gradient()
+
+
+init = torch.cat([mymanifold.MAP, torch.tensor([0, 0.1])]).to("cpu").detach().numpy()
+solution = solve_ivp(mymanifold.ode_fun, [0, 1], init, dense_output=True, rtol=1e-10, atol=1e-10)
+
+
+sol = mymanifold.expmap(mymanifold.MAP, torch.tensor([0, 0.1]))
+sol.y[:,-1]
+dir(sol)
+sol.y
+sol.t
+
+
+mymanifold.LA.sample(n_samples=1).shape
+
+mymanifold.MAP
+mymanifold.MAP_covariance
+mymanifold.gradient()
+mymanifold.loss()
+mymanifold.MAP + torch.tensor([0,0.1])
+
+mymanifold.set_weights(mymanifold.MAP + torch.tensor([0,0.1]))
+print(mymanifold.covariance())
+print(mymanifold.hess().inverse())
+print(mymanifold.LA.posterior_scale**2)
+
+mymanifold.set_to_map()
+print(mymanifold.covariance())
+print(mymanifold.hess().inverse())
 
