1c8e517f3333d82342fd947992a3352c5923a1f7
simoneiriksson <simoneiriksson@gmail.com>
2024-11-05 17:59:01 +0100

    Added get_parameter_vector_indices

--- riemannian-la/utils.py
+++ riemannian-la/utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import torch
 # Kindly copied from https://github.com/wjmaddox/drbayes/blob/master/subspace_inference/utils.py
 def set_weights(model, vector, device=None):
     offset = 0
@@ -18,12 +18,38 @@ def set_weights_old(params, w, device):
 
 # Kindly copied from https://github.com/wjmaddox/drbayes/blob/master/subspace_inference/utils.py
 def extract_parameters(model):
-    params = []	
-    for module in model.modules():	
-        for name in list(module._parameters.keys()):	
-            if module._parameters[name] is None:	
-                continue	
-            param = module._parameters[name]	
-            params.append((module, name, param.size()))	
-            module._parameters.pop(name)	
+    params = []
+    for module in model.modules():
+        for name in list(module._parameters.keys()):
+            if module._parameters[name] is None:
+                continue
+            param = module._parameters[name]
+            params.append((module, name, param.size()))
+            module._parameters.pop(name)
     return params
+
+# Function that loop over a list of param_names and return the indices of the parameters in the flattened vector
+def get_parameter_vector_indices(model, param_names):
+    """
+    Returns the index range of a specified parameter in the flattened parameter vector.
+
+    Args:
+        model (torch.nn.Module): The model containing parameters.
+        param_name (str): The name of the parameter for which to find indices.
+
+    Returns:
+        tuple: A tuple (start_index, end_index) representing the indices
+               of the specified parameter in the flattened vector, or None if not found.
+    """
+    start_idx = 0
+    indices = []
+    for name, param in model.named_parameters():
+        num_param_elements = param.numel()  # Total elements in this parameter tensor
+        end_idx = start_idx + num_param_elements  # End index is start + size of this parameter
+
+        if name in param_names:
+            indices.append(torch.arange(start_idx, end_idx))  # Return indices if parameter name matches
+
+        start_idx = end_idx  # Update start index for next parameter
+
+    return indices  # Return None if the parameter name is not found
