6df311e59eacdf0821c1caa67a9f0eeafd8b6813
simoneiriksson <simoneiriksson@gmail.com>
2025-01-12 15:02:08 +0100

    Done with hessian stuff

--- riemannian-la/hessian.py
+++ riemannian-la/hessian.py
@@ -1,8 +1,10 @@
-# this code takes a model and a dataset and computes the hessian of the loss function
-#
+"""
+This module contains functions to compute the hessian of a function at a point, 
+and the hessian of a model at a point
+"""
+
 import torch
 from torch.func import grad, jvp, vjp, hessian, jacfwd, jacrev, vmap, functional_call
-
 
 
 def hessian_from_func(func, x):
@@ -34,61 +36,40 @@ def hessian_from_model_loss_and_data(model, loss_func, xs, ys):
   H = H_func(params, xs, ys)
   return H
 
-
 def hessian_dict_to_matrix(hess_dict, verbose=False, device="cpu"):
     hess_size=0
-    # get parameter sizes:
-
     parameter_properties = {}
+    # The hessian dictionary is a dictionary of dictionaries. The outer dictionary has the parameter names as keys. 
+    # The inner dictionary has the parameter names as keys and the hessian tensor object as values.
+    # the hesian tensor object is a tensor that has the dimensionality = (parameter dimensionality + parameter dimensionality)
+    # So if the parameter is the weight matrix of a linear layer, the hessian tensor object will have the shape (weight_dim, weight_dim, weight_dim, weight_dim)
 
+    # Get parameter properties 
     for key1 in hess_dict.keys():
-        mat = hess_dict[key1][key1]
-        param_dims = len(mat.shape)//2
-        param_shape = mat.shape[0:param_dims]
-        param_numel = torch.prod(torch.tensor(param_shape)).item()
+        mat = hess_dict[key1][key1]  # take the diagonal element. This element is a tensor that has the dimensionality = parameter dimensionality + parameter dimensionality 
+        param_dims = len(mat.shape)//2  # get the number of dimensions of the parameter tensor
+        param_shape = mat.shape[0:param_dims]  # get the shape of the parameter tensor (which is the first half of the dimensions of the diagonal element)
+        param_numel = torch.prod(torch.tensor(param_shape)).item()  # get the number of elements in the parameter
         param_name = key1
-        parameter_properties[param_name] = {'param_shape': param_shape, 'param_dims': param_dims, 'param_numel': param_numel}
+        parameter_properties[param_name] = {'param_shape': param_shape, 'param_dims': param_dims, 'param_numel': param_numel}  # save information in dict
         hess_size += param_numel
-
     hess_matrix = torch.zeros((hess_size, hess_size), device=device)
     index1 = 0
     index2 = 0
+    # loop over the hessian dictionary and fill in the hessian matrix
     for key1 in hess_dict.keys():
         numel1 = parameter_properties[key1]['param_numel']
         dim1 = parameter_properties[key1]['param_dims']
         for key2 in hess_dict[key1].keys():
-            if verbose:
-                print(f"\n\n{key1 = }, {key2 = }")
-                print(f"{index1 = }, {index2 = }")
             local_hess = hess_dict[key1][key2]
-            if verbose:
-                print(f"{local_hess.shape = }") 
-                print(f"{local_hess = }")
-                print(f"{parameter_properties[key1]['param_shape'] = }")
-                print(f"{parameter_properties[key2]['param_shape'] = }")
-
             numel2 = parameter_properties[key2]['param_numel']
-            dim2 = parameter_properties[key2]['param_dims']
-            if verbose:
-                print(f"{numel1 = }, {numel2 = }")
-                print(f"{dim1 = }, {dim2 = }")
-
             flatten_key1 = torch.flatten(local_hess, start_dim=0, end_dim=dim1-1)
-            if verbose:
-                print(f"{flatten_key1.shape = }")
             local_hess_flat = torch.flatten(flatten_key1, start_dim=1)
-            if verbose:
-                print(f"{local_hess_flat.shape = }")
-
             hess_matrix[index1:index1+numel1, index2:index2+numel2] = local_hess_flat
-            if verbose:
-                print(f"{local_hess_flat = }")
-
             index2 += numel2
         index1 += numel1
         index2 = 0
     return hess_matrix, parameter_properties
-
 
 if __name__ == "main":
   # run a test of hessian_from_func
@@ -138,6 +119,7 @@ if __name__ == "main":
 
   H, params = hessian_dict_to_matrix(H_dict)
   print(f"{H = }") # should return a 2x2 matrix, With 5. in the counterdiagonal and zero in the diagonal
+  # tensor([[0., 5.], [5., 0.]])
 
   for param in params.keys():
     print(f"{param = }")
