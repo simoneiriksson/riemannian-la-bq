541765c617e13cbd25e5fa32245435f616897795
simoneiriksson <simoneiriksson@gmail.com>
2025-01-13 15:21:52 +0100

    change name from riemannian-la to riemannian_la

--- riemannian_la/getdata.py
+++ riemannian_la/getdata.py
@@ -72,31 +72,3 @@ def gen_log_regression_data(num_train_samples=10,
                 x[class_mask] = mvn.sample((num_class_samples,))
     return make_loaders(x, y, num_train_samples, batch_size)    
 
-if __name__ == "main":
-    # test the data generation functions
-    # Linear regression:
-    train_loader, test_loader = gen_linear_regression_data(num_train_samples=100)
-    X, y = next(iter(train_loader))
-    plt.scatter(X, y)
-
-    # Logistic regression, 1d:
-    train_loader, test_loader = gen_log_regression_data(num_train_samples=100, 
-                              num_test_samples=10, 
-                              num_features = 1,
-                              num_classes = 4,
-                              batch_size=0)
-    X, y = next(iter(train_loader))
-    plt.scatter(X, y, c=y)
-
-    # Logistic regression, 2d:
-    train_loader, test_loader = gen_log_regression_data(num_train_samples=100, 
-                              num_test_samples=10, 
-                              num_features = 2,
-                              num_classes = 3,
-                              seed = 2,
-                              variance=.1,
-                              batch_size=0)
-    X, y = next(iter(train_loader))
-    plt.scatter(X[:,0], X[:,1], c=y)
-
-
