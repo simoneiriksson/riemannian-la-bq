7dc9b6792c9a6e0319ed77ea9c85e7e48817eab0
simoneiriksson <simoneiriksson@gmail.com>
2025-01-14 18:46:05 +0100

    fixed randomization in data generation

--- riemannian_la/getdata.py
+++ riemannian_la/getdata.py
@@ -36,15 +36,30 @@ def make_loaders(X, y, train_size, batch_size=0):
     test_loader = DataLoader(subset_test, batch_size=batch_size)
     return train_loader, test_loader
 
-def gen_linear_regression_data(num_train_samples=10, 
-                              num_test_samples=10, 
-                              noise_std=1.0, 
-                              batch_size=0):
+def gen_polynomial_regression_data(weights = torch.tensor([2.0, 1.0]),
+                                   num_train_samples=10, 
+                                   num_test_samples=10, 
+                                   noise_std=1.0, 
+                                   batch_size=0, seed=2):
     def fn(x):
-        return 2 - 1*x 
+        paws = torch.arange(len(weights), device=x.device).float()
+        return weights * x**paws
     N = num_train_samples + num_test_samples
     X = torch.rand(N, 1)*2-1
     y = fn(X) + torch.randn_like(X) * noise_std
+    return make_loaders(X, y, num_train_samples, batch_size)    
+
+
+def gen_linear_regression_data(num_train_samples=10, 
+                              num_test_samples=10, 
+                              target_sigma=1.0, 
+                              batch_size=0, seed=2):
+    def fn(x):
+        return 2 - 1*x 
+    N = num_train_samples + num_test_samples
+    with torch_seed(seed):
+        X = torch.rand(N, 1)*2-1
+        y = fn(X) + torch.randn_like(X) * target_sigma
     return make_loaders(X, y, num_train_samples, batch_size)    
 
 def gen_log_regression_data(num_train_samples=10, 
@@ -52,9 +67,9 @@ def gen_log_regression_data(num_train_samples=10,
                               num_features = 1,
                               num_classes = 2,
                               variance = .1,
-                              batch_size=0):
+                              batch_size=0, seed=2):
     num_samples = num_train_samples + num_test_samples
-    with torch_seed(2):
+    with torch_seed(seed):
         means = torch.randn(num_classes, num_features)
         class_weights = torch.nn.functional.softmax(torch.randn(num_classes))
         covariances = torch.eye(num_features).repeat(num_classes, 1, 1)*variance
