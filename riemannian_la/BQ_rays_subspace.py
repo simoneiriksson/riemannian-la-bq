e32507cf652c53eb1f733fed09033beca075044b
simoneiriksson <simoneiriksson@gmail.com>
2025-02-07 23:55:23 +0100

    Worked out function tranformations to ensure positive variance

--- riemannian_la/BQ_rays_subspace.py
+++ riemannian_la/BQ_rays_subspace.py
@@ -1,10 +1,10 @@
 import torch
 from torch.utils.data import DataLoader, TensorDataset
 from torch.distributions.multivariate_normal import _precision_to_scale_tril
-from utils import tensify, loss_func_from_target_sigma, make_functional_fwd_xs, vector_to_parameterdict, make_functional_fwd
+from utils import tensify, loss_func_from_target_sigma, make_functional_fwd_xs, vector_to_parameterdict, make_functional_fwd, make_functional_fwd_vector_xs
 from GGN_hessian import GGN_hessian_from_loader
 from hessian import hessian_from_model_loss_and_data, hessian_dict_to_matrix, hessian_from_loader, hessian_from_func
-from riemannian_la.utils import NegLogLik_regression, NegLogLik_classification, iid_gaussian_prior_loss
+from utils import NegLogLik_regression, NegLogLik_classification, iid_gaussian_prior_loss
 from torch.func import grad, jvp, vjp, hessian, jacfwd, jacrev, vmap, functional_call
 from laplace_approx import Laplace
 from scipy.integrate import solve_ivp
@@ -20,10 +20,13 @@ from riemann_sampler import Riemann_sampler, riemann_plotter
 import GPy
 from FullGaussianMeasure import FullGaussianMeasure
 from emukit.model_wrappers.gpy_quadrature_wrappers import BaseGaussianProcessGPy, RBFGPy
+from emukit.model_wrappers import GPyModelWrapper
+
 from emukit.quadrature.kernels import QuadratureRBFLebesgueMeasure, QuadratureRBFGaussianMeasure
 from emukit.quadrature.measures import LebesgueMeasure, GaussianMeasure
 
-from emukit.quadrature.methods import VanillaBayesianQuadrature
+from emukit.quadrature.methods import VanillaBayesianQuadrature, WarpedBayesianQuadratureModel, BoundedBayesianQuadrature
+from emukit.quadrature.methods.warpings import SquareRootWarping
 from emukit.quadrature.acquisitions import IntegralVarianceReduction
 from emukit.core.optimization import GradientAcquisitionOptimizer
 from emukit.core.parameter_space import ParameterSpace
@@ -37,6 +40,59 @@ def reasonable_box_fixed(self_object, factor=4):
         upper = self_object.mean + factor * np.sqrt(self_object.variance)
         return list(zip(lower, upper))
     return fn
+
+
+
+def transform(type="sqrt", param=1):
+    if type == "sqrt":
+        def fn_forward(x):
+            return (x).sqrt()
+        def fn_backward(mus, vars):
+            return [mus**2 + vars]
+        
+    elif type == "log":
+        def fn_forward(x):
+            return (x).log()
+        def fn_backward(mus, vars):
+            return [torch.exp(mus + .5*vars)]
+        
+    elif type == "softplus":
+        def fn_forward(x):
+            return (x+param).log()
+        def fn_backward(mus, vars):
+            return [(mus + 0.5*vars).exp()-param]
+        
+    elif type == "identity":
+        def fn_forward(x):
+            return x
+        def fn_backward(mus, vars):
+            return [mus, vars]
+        
+    elif type == "exp":
+        def fn_forward(x):
+            return x.exp()
+        def fn_backward(mus, vars):
+            return [mus.log()]
+
+    elif type == "inv":
+        def fn_forward(x):
+            return 1/x
+        def fn_backward(mus, vars):
+            return [1/mus]
+        
+    elif type == "softexp":
+        def fn_forward(x):
+            return (x).exp()-param
+        def fn_backward(mus, vars):
+            return [(mus + vars*.5).exp()-param]
+        
+    elif type == "power":
+        def fn_forward(x):
+            return (x)**(param)
+        def fn_backward(mus, vars):
+            return [(mus**2)**(1/(param*2)) + .5*vars]
+
+    return fn_forward, fn_backward
 
 
 
@@ -59,6 +115,7 @@ class BayesianQuadrature_rays():
 
 
         self.functional_fwd = make_functional_fwd_vector(evaluation_model, xs, parametersubset=self.parametersubset)
+        self.functional_fwd_xs = make_functional_fwd_xs(evaluation_model)
 
         self.num_timesteps = num_timesteps
         self.square_plots = square_plots
@@ -97,9 +154,9 @@ class BayesianQuadrature_rays():
                                     input_dim=self.sampling_dims, 
                                     lengthscale=self.GP_lengthscale,
                                     variance=self.GP_variance)
-        print(f"{self.vs.shape = }, {self.integrand_values.shape = }")
-        self.gpy_model = GPy.models.GPRegression(X=self.vs[0:1], 
-                                                 Y=self.integrand_values[0:1], 
+        #print(f"{self.vs.shape = }, {self.integrand_values.shape = }")
+        self.gpy_model = GPy.models.GPRegression(X=self.vs_ray, 
+                                                 Y=self.integrand_values, 
                                                  kernel=self.BQ_kernel)
         self.emukit_rbf = RBFGPy(self.gpy_model.kern)
 
@@ -127,8 +184,8 @@ class BayesianQuadrature_rays():
             self.plt_measure_dist = torch.distributions.MultivariateNormal(torch.zeros(self.sampling_dims), torch.eye(self.sampling_dims))
 
         if self.measure_type=="lebesgue":
-            print(f"{self.limits = }")
-            print(f"{self.plot_limits = }")
+            #print(f"{self.limits = }")
+            #print(f"{self.plot_limits = }")
             limits_list_of_lists = [[-l, l] for l in self.limits]
             self.emukit_measure = LebesgueMeasure.from_bounds(limits_list_of_lists)
             self.emukit_qrbf = QuadratureRBFLebesgueMeasure(self.emukit_rbf, self.emukit_measure)
@@ -136,7 +193,9 @@ class BayesianQuadrature_rays():
             self.plt_measure_dist.log_prob = lambda z: -torch.log((torch.tensor(self.plot_limits) * 2).prod()).repeat(z.shape[0])
 
         self.emukit_model = BaseGaussianProcessGPy(kern=self.emukit_qrbf, gpy_model=self.gpy_model)
-        self.emukit_method = VanillaBayesianQuadrature(base_gp=self.emukit_model, X=self.vs[0:1], Y=self.integrand_values[0:1])
+        #self.emukit_method = VanillaBayesianQuadrature(base_gp=self.emukit_model, X=self.vs[0:1], Y=self.integrand_values[0:1])
+        print(f"{self.vs_ray.shape = }, {self.integrand_values.shape = }")
+        self.emukit_method = VanillaBayesianQuadrature(base_gp=self.emukit_model, X=self.vs_ray, Y=self.integrand_values)
         if use_ray_acqusition:
             self.ivr_acquisition = RayAcquisition(self.emukit_method, self.v_init, self.num_timesteps)
         else:
@@ -178,7 +237,6 @@ class BayesianQuadrature_rays():
     def lebesgue_integrand(self, v):
         num_ts = self.num_timesteps
         res, ts = self.Rsampler.expmap_torchdiffeq(self.Rsampler.mean, self.Rsampler.scale @ v, num_ts=num_ts)
-        #theta = res[-1,:self.Rsampler.num_params].T
         thetas = res[:,:self.Rsampler.num_params]
         vs_ray = torch.tensor(np.linspace(self.v_init, v.detach().numpy(), self.num_timesteps))
         v_dist = torch.distributions.MultivariateNormal(torch.zeros(self.sampling_dims), torch.eye(self.sampling_dims))
@@ -214,10 +272,62 @@ class BayesianQuadrature_rays():
         ys = torch.stack([self.functional_fwd(theta) for theta in thetas])
         return ys, thetas
 
+    def predict_samples(self, xs):
+        for i, theta in enumerate(self.thetas):
+            params = vector_to_parameterdict(torch.tensor(theta, dtype=torch.float32), self.parametersubset)
+            pred = self.functional_fwd_xs(params, xs)
+            if i == 0:
+                preds = torch.zeros((len(self.thetas), *pred.shape))
+            preds[i] = pred
+        return preds.permute(1,0,2)
+    
+    def pred_BQ(self, xs, get_mean=True, get_var=True, get_measure_variance=False, transform_type = "power", transform_param=2):
+        preds = self.predict_samples(xs)
+        X_bck = self.emukit_method.X
+        Y_bck = self.emukit_method.Y
+        # get means
+        for i, pred in enumerate(preds):
+            self.emukit_method.set_data(self.vs_ray, pred)
+            mu, var = self.emukit_method.integrate()
+            if i == 0:
+                first_moments_mus = torch.zeros((len(preds), *mu.shape))
+                first_moments_vars = torch.zeros((len(preds), *var.shape))
+            first_moments_mus[i] = mu
+            first_moments_vars[i] = var
+        
+        forward_transform, backwards_transform = transform(transform_type, param=transform_param)
+
+        # get second moment
+        for i, pred in enumerate(preds):
+            sqr_error = (preds[i] -first_moments_mus.unsqueeze(1).unsqueeze(2)[i])**2
+            emukit_method = VanillaBayesianQuadrature(base_gp=self.emukit_model, X=self.vs_ray, Y=forward_transform(sqr_error))
+            mu, var = emukit_method.integrate()
+            if i == 0:
+                central_second_moment_mus = torch.zeros((len(preds), *mu.shape))
+                central_second_moment_vars = torch.zeros((len(preds), *var.shape))
+            trans = backwards_transform(torch.tensor(mu), torch.tensor(var))
+            central_second_moment_mus[i] = trans[0]
+            if len(trans) == 2: 
+                central_second_moment_vars[i] = trans[1]
+        
+        self.emukit_method.set_data(X_bck, Y_bck)
+
+        vars = [first_moments_vars]
+        if len(trans)==2: vars += [central_second_moment_vars]
+
+        returns = []
+        if get_mean:
+            returns += [first_moments_mus]
+        if get_var:
+            returns += [central_second_moment_mus]
+        if get_measure_variance:
+            returns += [vars]
+        return returns
+
     def plot_integrand_2d(self, ax=None):
         # plot integrand values
         ax.scatter(self.vs_ray[:,0], self.vs_ray[:,1], c=self.integrand_values, cmap="viridis")
-        ax.set_title("Integrant values")
+        ax.set_title("Integrand values")
         ax.set_xlim(-self.plot_limits[0], self.plot_limits[0])
         ax.set_ylim(-self.plot_limits[1], self.plot_limits[1])
         
@@ -225,7 +335,7 @@ class BayesianQuadrature_rays():
         ax.scatter(self.vs_ray[:,0], self.vs_ray[:,1], 
                     c=self.integrand_values[:,0] * self.plt_measure_dist.log_prob(torch.tensor(self.vs_ray)).exp().detach().numpy(), 
                     cmap="viridis")
-        ax.set_title("Integrant values times measure")
+        ax.set_title("Integrand values times measure")
         ax.set_xlim(-self.plot_limits[0], self.plot_limits[0])
         ax.set_ylim(-self.plot_limits[1], self.plot_limits[1])
 
@@ -262,22 +372,48 @@ class BayesianQuadrature_rays():
 
     def plot_true_function_likelihood_2d(self, ax=None):
         self.plot_init_2d()
+        self.get_funcvals()
         ax.contourf(self.xs_plt, self.ys_plt, self.function_times_likelihood.detach(), cmap="viridis")
         ax.scatter(self.thetas[:,0] - self.Rsampler.mean[0].detach().numpy(), self.thetas[:,1] - self.Rsampler.mean[1].detach().numpy(), c="k", marker="x")
+        
+        rays = self.thetas[1:].reshape(-1, self.num_timesteps-1, 2).transpose(0, 2, 1)
+        rays2 = np.concatenate([np.tile(self.thetas[None, 0, None].transpose(0, 2, 1), [rays.shape[0],1,1]), rays] , axis=2)
+
+        for i, sample in enumerate(rays2):
+            ax.plot(sample[0]-self.Rsampler.mean[0].detach().numpy(), 
+                    sample[1]-self.Rsampler.mean[1].detach().numpy(), marker=None, c="k", alpha=0.3)
+
         ax.set_title("True function times model likelihood")
         ax.set_xlim(-self.plot_limits[0], self.plot_limits[0])
         ax.set_ylim(-self.plot_limits[1], self.plot_limits[1])
+        if self.theta_space_plot_limits is not None:
+            ax.set_xlim(self.theta_space_plot_limits[0])
+            ax.set_ylim(self.theta_space_plot_limits[1])
+
 
     def plot_model_likelihood_2d(self, ax=None):
         self.plot_init_2d()
+        self.get_funcvals()
         ax.contourf(self.xs_plt, self.ys_plt, (-self.plt_model_loss*2).exp().detach(), cmap="viridis")
         ax.contour(self.xs_plt, self.ys_plt, self.function_vals.detach(), cmap="Grays")
         
         ax.scatter(self.thetas[:,0] - self.Rsampler.mean[0].detach().numpy(), self.thetas[:,1] - self.Rsampler.mean[1].detach().numpy(), c="k", marker="x")
+
+        rays = self.thetas[1:].reshape(-1, self.num_timesteps-1, 2).transpose(0, 2, 1)
+        rays2 = np.concatenate([np.tile(self.thetas[None, 0, None].transpose(0, 2, 1), [rays.shape[0],1,1]), rays] , axis=2)
+
+        for i, sample in enumerate(rays2):
+            ax.plot(sample[0]-self.Rsampler.mean[0].detach().numpy(), 
+                    sample[1]-self.Rsampler.mean[1].detach().numpy(), marker=None, c="k", alpha=0.3)
+
+
         #axes[0][3].contour(self.xs_plt, self.ys_plt, self.function_vals.detach(), c="k")
         ax.set_title("Model loss and true function values")
         ax.set_xlim(-self.plot_limits[0], self.plot_limits[0])
         ax.set_ylim(-self.plot_limits[1], self.plot_limits[1])
+        if self.theta_space_plot_limits is not None:
+            ax.set_xlim(self.theta_space_plot_limits[0])
+            ax.set_ylim(self.theta_space_plot_limits[1])
 
 
     def plot_init_2d(self):
@@ -285,9 +421,10 @@ class BayesianQuadrature_rays():
             self.xs_plt = np.linspace(-self.plot_limits[0], self.plot_limits[0], self.plot_N_mesh)
             self.ys_plt = np.linspace(-self.plot_limits[1], self.plot_limits[1], self.plot_N_mesh)
             self.xys_plt = np.array([[x, y] for x in self.xs_plt for y in self.ys_plt])
-
+    
+    def get_funcvals(self):
+        if not self.has_plotted:
             self.function_vals = torch.stack([self.functional_fwd(obs + self.Rsampler.mean) for obs in torch.tensor(self.xys_plt, dtype=torch.float32)]).reshape(self.plot_N_mesh, self.plot_N_mesh).T
-            
             #f_model = make_functional_fwd_vector(self.Rsampler.model, self.xs, parametersubset=dict(self.Rsampler.model.named_parameters()))
             #f_model = make_functional_fwd_xs(self.Rsampler.model)
             #self.f_loss = functional_loss_for_vmap(f_model, self.Rsampler.parametersubset, self.Rsampler.loss_fn, self.Rsampler.xs, self.Rsampler.ys, prior_logprob=self.Rsampler.prior_logprob)
@@ -304,7 +441,7 @@ class BayesianQuadrature_rays():
         plt_measure = self.plt_measure_dist.log_prob(torch.tensor(xs_plt)).exp().detach().numpy()
         plt_measure_points = self.plt_measure_dist.log_prob(torch.tensor(self.vs_ray)).exp().detach().numpy()
         ax.fill_between(xs_plt[:,0], (mu_plot - np.sqrt(var_plot))[:,0]* plt_measure, (mu_plot + np.sqrt(var_plot))[:,0]* plt_measure, alpha=0.5, label="BQ std")
-        ax.scatter(self.vs_ray[:,0], self.integrand_values[:,0] * plt_measure_points , c="k", marker="x", label="Integrant times measure")
+        ax.scatter(self.vs_ray[:,0], self.integrand_values[:,0] * plt_measure_points , c="k", marker="x", label="Integrand times measure")
         ax.plot(xs_plt[:,0], mu_plot[:,0] * plt_measure, label="BQ mean times measure", c="b")
         ax.plot(xs_plt[:,0], plt_measure, label="Measure", c="g")
         ax.set_title("Integrand times measure")
@@ -316,7 +453,7 @@ class BayesianQuadrature_rays():
         xs_plt = np.linspace(-self.limits, self.limits, self.plot_N_mesh)
         mu_plot, var_plot = self.emukit_method.predict(xs_plt)
         ax.fill_between(xs_plt[:,0], (mu_plot - np.sqrt(var_plot))[:,0], (mu_plot + np.sqrt(var_plot))[:,0], alpha=0.5, label="BQ std")
-        ax.scatter(self.vs_ray[:,0], self.integrand_values, c="k", marker="x", label="Integrant")
+        ax.scatter(self.vs_ray[:,0], self.integrand_values, c="k", marker="x", label="Integrand")
         ax.plot(xs_plt, mu_plot, label="BQ mean", c="b")
         ax.set_title("Integrand values")
         #ax.set_xlim(-self.limits[0], self.limits[0])
@@ -329,7 +466,7 @@ class BayesianQuadrature_rays():
         ax.plot(self.xs_plt_1d[:,0], self.function_vals.detach().numpy(), label="True function")
         ax.plot(self.xs_plt_1d[:,0], self.plt_likelihood.detach().numpy(), label="Model likelihood")
 
-        ax.scatter(self.thetas[:,0] - self.Rsampler.mean[0].detach().numpy(), self.integrand_values, c="k", marker="x", label="Integrant")
+        ax.scatter(self.thetas[:,0] - self.Rsampler.mean[0].detach().numpy(), self.integrand_values, c="k", marker="x", label="Integrand")
         ax.set_title("True function times model likelihood")
         ax.set_xlim(-self.limits, self.limits)
         ax.set_ylim(min(self.function_times_likelihood.nan_to_num(posinf=10).min().detach(), self.integrand_values.min()), 
@@ -363,13 +500,13 @@ class BayesianQuadrature_rays():
             return fig, axes
 
         if self.Rsampler.num_params not in  [1,2] and self.sampling_dims == 2: # This is the case where we have >2d parameters and 2d subspace
-            fig, axes = plt.subplots(2,4, figsize=(20,10))
+            fig, axes = plt.subplots(2,3, figsize=(15,10))
             self.plot_integrand_2d(ax=axes[0][0])
             self.plot_integrand_measure_2d(ax=axes[0][1])
             self.plt_BQ_mean_2d(ax=axes[1][0])
             self.plot_BQ_mean_measure_2d(ax=axes[1][1])
             self.plot_BQ_std_2d(ax=axes[1][2])
-            self.plot_acquisition_2d(ax=axes[1][3])
+            self.plot_acquisition_2d(ax=axes[0][2])
             self.has_plotted = True
             return fig, axes
 
